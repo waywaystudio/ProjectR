b921b866a79fbd01b99ae7b51b7642a668a51d40
Waywaydev <wayway.dev@gmail.com>
2022-11-01 00:48:19 +0900

    [GameObject LINQ] Import External Asset & Delete MEC - TimingManager

--- Assets/Project/Sandbox/CoroutineTester.cs
+++ Assets/Project/Sandbox/CoroutineTester.cs
@@ -1,16 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class CoroutineTester : MonoBehaviour
 {
-    [PropertyRange(0, 10)]
-    [SerializeField] private int testerValue;
-    
-    [Button]
-    public void CoroutineTest()
-    {
-        
-    }
+
 }
