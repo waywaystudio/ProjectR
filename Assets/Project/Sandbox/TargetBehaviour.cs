dd5c805e2584808af6407463213172ff171764fa
Waywaydev <wayway.dev@gmail.com>
2022-11-19 01:21:32 +0900

    _etc. Small Test

--- Assets/Project/Sandbox/TargetBehaviour.cs
+++ Assets/Project/Sandbox/TargetBehaviour.cs
@@ -13,13 +13,13 @@ public class TargetBehaviour : MonoBehaviour
     
     private void Update()
     {
-        // if (!Input.GetMouseButtonDown(0)) return;
-        //
-        // var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        //
-        // if (!Physics.Raycast(ray, out var hit)) return;
-        //
-        // newPosition = hit.point;
-        // transform.position = newPosition;
+        if (!Input.GetMouseButtonDown(0)) return;
+        
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        
+        if (!Physics.Raycast(ray, out var hit)) return;
+        
+        newPosition = hit.point;
+        transform.position = newPosition;
     }
 }
