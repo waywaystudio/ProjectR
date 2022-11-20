45b36d3ca9c38f869038d0b02185f7869378e459
Waywaydev <wayway.dev@gmail.com>
2022-11-20 15:04:27 +0900

    [Behavior Tree] Import

--- Assets/Project/Scripts/Contents/Raid/RaidDirector.cs
+++ Assets/Project/Scripts/Contents/Raid/RaidDirector.cs
@@ -7,21 +7,12 @@ namespace Raid
     public class RaidDirector : MonoBehaviour
     {
         [SerializeField] private RaidCameraDirector cameraDirector;
-        [SerializeField] private GameObject playerPrefab;
-        [SerializeField] private Transform hierarchy;
-        [SerializeField] private Transform spawnPoint;
 
         private void Awake()
         {
             cameraDirector ??= GetComponent<RaidCameraDirector>();
             
-            var player = Instantiate(playerPrefab, hierarchy);
-            var position = spawnPoint.position;
-            var rotation = spawnPoint.rotation;
             
-            player.transform.SetPositionAndRotation(position, rotation);
-
-            cameraDirector.SetPlayerCameraFocus(player.transform);
             MainGame.UI.FadePanel.PlayFadeIn();
         }
     }
